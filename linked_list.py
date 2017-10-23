84614b49a8dcb3b61181593dab916ac7a7dec11e
Nathan Moore <ncmoore1986@gmail.com>
2017-10-23 14:20:44 -0700

    added a few more methods

--- linked_list.py
+++ linked_list.py
@@ -2,7 +2,7 @@
 
 
 class Node(object):
-    """Define Node objects."""
+    """Define Node-class objects."""
 
     def __init__(self, data, next):
         """Initiate a new instance of a Node object with attributes."""
@@ -17,3 +17,33 @@ class Node(object):
         """Write something here."""
         self.data = data
         return self.data
+
+    def set_next(self, next):
+        """Define what the next node is."""
+        self.next = next
+        return self.next
+
+    def get_next(self, next):
+        """Return what the next node is."""
+        return self.next
+
+
+class LinkedList(object):
+    """Define LinkedList-class objects."""
+
+    def __init__(self):
+        """Initiate a new instance of a LinkedList object with attributes."""
+        self.head = ""
+
+    def size(self):
+        """Define the size method of the LinkedList-class object."""
+        current_node = self.head
+        count = 0
+        while current_node:
+            count += 1
+            current_node = current_node.get_next()
+        return count
+
+    def push(self, val):
+        """Define the push method for LinkedList-class object."""
+

50f6b520a049866b8e982733ebef6758c6be0383
David Franklin <dave5801@gmail.com>
2017-10-23 13:37:48 -0700

    created node class

---
+++ linked_list.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data)
+        self.data = data

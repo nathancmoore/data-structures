29354be63de426d065070b6ecbcff5a996070531
David Franklin <dave5801@gmail.com>
2017-10-23 15:02:22 -0700

    test search method

--- test_linked_list.py
+++ test_linked_list.py
@@ -23,3 +23,17 @@ def test_node_object_exists():
     from linked_list import Node
     node = Node(0, 1)
     assert node
+
+def test_search_found():
+    """Test that push value to list then search for it """
+    from linked_list import LinkedList
+    test_linked_list = LinkedList()
+    test_linked_list.push(20)
+    assert test_linked_list.search(20)
+
+def test_search_not_fount():
+    """Test that will search for value not in list """
+    from linked_list import LinkedList
+    test_linked_list = LinkedList()
+    assert test_linked_list.search(5) == None
+    

5d1612ed1363cbc48aa05b007827fb16583aae8a
David Franklin <dave5801@gmail.com>
2017-10-26 17:12:58 -0700

    added dequeue, peek, and size methods

--- que_.py
+++ que_.py
@@ -3,6 +3,7 @@
 from doubly_linked_list import DLinkedList
 from doubly_linked_list import Node
 
+
 class Queue(object):
     """Queue Class which is composed from Linked List."""
 
@@ -11,10 +12,10 @@ class Queue(object):
         self.linked_list = DLinkedList()
 
     def enqueue(self, val):
-
+        """Add new element to end of the queue."""
         new_node = Node(self, val)
 
-        if self.linked_list.head == None:
+        if self.linked_list.head is None:
             self.linked_list.head = new_node
             self.linked_list.tail = new_node
         else:
@@ -22,10 +23,19 @@ class Queue(object):
             new_node.next_node = self.linked_list.tail
             self.linked_list.tail == new_node
 
+    def dequeue(self):
+        """Remove from the end of the queue."""
+        if self.linked_list.head is not None:
+            removed_node = self.linked_list.head
+            self.linked_list.head.prev_node = self.linked_list.head
+            return removed_node.data
+        raise IndexError("List is empty")
 
-    def dequeue():
+    def peek(self):
+        """Return value of next element in queue."""
+        return self.linked_list.head
 
-    def peek():
-
-    def size():
-
+    def size(self):
+        """Define the size method."""
+        return self.linked_list.size()
+        

16c9c54aa5c44d87e31869454305af09db5b1cae
Nathan Moore <ncmoore1986@gmail.com>
2017-10-30 13:38:08 -0700

    wrote the append and appendleft methods.

--- deque.py
+++ deque.py
@@ -1,10 +1,10 @@
 """Define new Class for Node, Linked List, and Dequeue."""
 """Node and Linked List have been rebuilt for practice."""
 
-class Node(object):
+class Node(object, val):
      """Define Node-class objects."""
     def __init__(self, data = None, next_node = None, prev_node = None):
-        self.data = data
+        self.data = val
         self.next_node = next_node
         self.prev_node = next_node
 
@@ -15,43 +15,50 @@ class QLinkedList(object):
         self.head = None
         self.tail = None
 
-    def size():
+    def size(self):
         """Define the size method of the QLinkedList-class object."""
         return ""
 
-    def push():
+    def push(self):
          """Define the push method for QLinkedList-class object."""
         return ""
     
-    def append():
-        """Define the append method for QLinkedList-class object."""
-        return ""
+    def append(self, val):
+        """Adds a node to the tail with a specified value."""
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
     
-    def remove():
+    def appendleft(self, val):
+        """Adds a node to the head with a specified value."""
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev_node = new_node
+            self.head = new_node
+
+    def remove(self):
         """Remove a node from the list."""
         return ""
     
-    def pop():
+    def pop(self):
         """Remove and returns the head node of the list."""
         return ""
 
-    def shift():
+    def shift(self):
         """Remove and return the tail node of the list."""
         return ""
 
-    def display():
+    def display(self):
         """Display unicode string of linked list contents."""
         return ""
 
-    def search():
+    def search(self):
         """Search the linked list for a node with a particular value."""
         return ""
-
-class Deque():
-    """Initialize class for Double Ended Queue."""
-    def __init__(self):
-         self.linked_list = QLinkedList()
-
-        
-
-

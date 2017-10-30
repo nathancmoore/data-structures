e1a33ae272a5f69780e0ba53494c706682a64402
Nathan Moore <ncmoore1986@gmail.com>
2017-10-30 13:58:43 -0700

    added a few more tests

--- test_deque.py
+++ test_deque.py
@@ -28,3 +28,35 @@ def test_appendleft_into_empty():
 
 def test_size_when_appending():
     """Test that size works when appending."""
+    from deque import QLinkedList
+    d = QLinkedList()
+    assert d.size() == 0
+    d.append(1)
+    assert d.size() == 1
+    d.append(2)
+    assert d.size() == 2
+
+
+def test_size_when_appendlefting():
+    """Test that size works when appendleft-ing."""
+    from deque import QLinkedList
+    d = QLinkedList()
+    assert d.size() == 0
+    d.appendleft(1)
+    assert d.size() == 1
+    d.appendleft(2)
+    assert d.size() == 2
+
+
+def test_pop_on_empty():
+    """Test that popping an empty list returns none."""
+    from deque import QLinkedList
+    d = QLinkedList()
+    assert d.pop() is None
+
+
+def test_popleft_on_empty():
+    """Test that popleft-ing an empty list returns none."""
+    from deque import QLinkedList
+    d = QLinkedList()
+    assert d.popleft() is None

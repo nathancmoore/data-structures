509de1efa99832487efd09b5a6d301d54d1dcb1d
David Franklin <dave5801@gmail.com>
2017-10-30 13:29:51 -0700

    created shell for deque

---
+++ deque.py
@@ -0,0 +1,57 @@
+"""Define new Class for Node, Linked List, and Dequeue."""
+"""Node and Linked List have been rebuilt for practice."""
+
+class Node(object):
+     """Define Node-class objects."""
+    def __init__(self, data = None, next_node = None, prev_node = None):
+        self.data = data
+        self.next_node = next_node
+        self.prev_node = next_node
+
+
+class QLinkedList(object):
+    def __init__(self):
+        """Initiate a new instance of a QLinkedList object with attributes."""
+        self.head = None
+        self.tail = None
+
+    def size():
+        """Define the size method of the QLinkedList-class object."""
+        return ""
+
+    def push():
+         """Define the push method for QLinkedList-class object."""
+        return ""
+    
+    def append():
+        """Define the append method for QLinkedList-class object."""
+        return ""
+    
+    def remove():
+        """Remove a node from the list."""
+        return ""
+    
+    def pop():
+        """Remove and returns the head node of the list."""
+        return ""
+
+    def shift():
+        """Remove and return the tail node of the list."""
+        return ""
+
+    def display():
+        """Display unicode string of linked list contents."""
+        return ""
+
+    def search():
+        """Search the linked list for a node with a particular value."""
+        return ""
+
+class Deque():
+    """Initialize class for Double Ended Queue."""
+    def __init__(self):
+         self.linked_list = QLinkedList()
+
+        
+
+

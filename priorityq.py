fae0ccb7eb2ba1a73434e66cf65d0a840e42882c
David Franklin <dave5801@gmail.com>
2017-11-01 13:19:21 -0700

    commit to priorityq

---
+++ priorityq.py
@@ -0,0 +1,24 @@
+"""This is the class for a priority queue."""
+import binheap
+"""."""
+class PriorityQ(object):
+    """Implementation of a Priority Queue using an existing binary heap."""
+    def __init__(self, val = None, priority = None)
+        self.val = val
+        self.binheap = BinHeap()
+
+    def insert(self,value):
+        """."""
+        return ""
+
+    def pop(self):
+        """."""
+        return ""
+
+    def peek(self):
+        """."""
+        return
+
+
+
+        """."""
---
+++ test_priorityq.py
@@ -0,0 +1,6 @@
+"""This is the test file."""
+
+import pytest
+
+ """test comment."""
+ """."""

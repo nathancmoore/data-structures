cb7f361ace141f6041a56c9def5e0b1d6c0d17bf
Nathan Moore <ncmoore1986@gmail.com>
2017-11-01 14:44:59 -0700

    wrote more tests

--- test_priorityq.py
+++ test_priorityq.py
@@ -22,8 +22,8 @@ def test_priorityq_object_exists(sample_priorityq):
     assert sample_priorityq
 
 
-def test_push_increases_size(sample_priorityq):
-    """Test that the push method increases the size of the heap."""
+def test_insert_increases_size(sample_priorityq):
+    """Test that the insert method increases the size of the queue."""
     assert len(sample_priorityq.heap_list) == 0
     sample_priorityq.insert([5, 1])
     assert len(sample_priorityq.heap_list) == 1
@@ -32,7 +32,7 @@ def test_push_increases_size(sample_priorityq):
 
 
 def test_pop_decreases_size(sample_priorityq):
-    """Test that the pop method decreases the size of the heap."""
+    """Test that the pop method decreases the size of the queue."""
     for i in range(5):
         sample_priorityq.insert([i, i + 3])
     sample_priorityq.pop()
@@ -59,3 +59,15 @@ def test_peek_always_returns_highest_priority(sample_priorityq):
     sample_priorityq.insert(["value3", 7])
     sample_priorityq.insert(["value4", 0])
     assert sample_priorityq.pop() == "value2"
+
+
+def test_pop_on_empty_raises_error(sample_priorityq):
+    """Test that the pop method always returns the highest priority node's value."""
+    with pytest.raises(IndexError):
+        sample_priorityq.pop()
+
+
+def test_peek_on_empty_raises_error(sample_priorityq):
+    """Test that the pop method always returns the highest priority node's value."""
+    with pytest.raises(IndexError):
+        sample_priorityq.peek()

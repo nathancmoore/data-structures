a9cf5a9071be51229a0e75741763a3779f2f508a
David Franklin <dave5801@gmail.com>
2017-10-31 17:11:53 -0700

    redo of binheap

--- binheap.py
+++ binheap.py
@@ -3,73 +3,58 @@
 
 class Node(object):
     """Define Node-class objects."""
+    def __init__(self, index=None, value=None):
+        self.index = index
+        self.value = value
+        self.left = 2 * self.index + 1
+        self.right = 2 * self.index + 2
 
-    def __init__(self, idx, data=None):
-        """Initiate a new instance of a Node object with attributes."""
-        self.data = data
-        self.idx = idx
-        self.lchild_idx = 2 * self.idx + 1
-        self.rchild_idx = 2 * self.idx + 2
-
-        if idx % 2 == 1:
-            self.parent_idx = (self.idx - 1) // 2
+        '''
+        if index % 2 == 1:
+            self.parent = (self.index - 1) // 2
         else:
-            self.parent_idx = (self.idx - 2) // 2
-
+            self.parent = (self.index - 2) // 2
+        '''
 
 class BinHeap(object):
     """Define the class of a max-Binary Heap."""
 
-    def __init__(self, head=None, starting_values=None):
-        """Initiate a new instance of a Node object with attributes."""
-        self.head = head
+    def __init__(self, root=None, iterable=()):
+         """Initiate a new instance of a Binheap object with attributes."""
+        self.heap_list = []
+        self.root = root
 
-        if starting_values:
-            self.node_list = []
-            idx = 0
-            for val in starting_values:
-                self.node_list.append(Node(idx, val))
-                idx += 1
-
-        else:
-            self.node_list = []
-
-    def _swap_nodes(self, node1, node2):
-        """Swap the position of two nodes in the heap."""
-        temp_node1 = [node1.idx, node1.parent, node1.lchild, node1.rchild]
-        temp_node2 = [node2.idx, node2.parent, node2.lchild, node2.rchild]
-        node1.idx = temp_node2[0]
-        node1.parent = temp_node2[1]
-        node1.lchild = temp_node2[2]
-        node1.rchild = temp_node2[3]
-        node2.idx = temp_node1[0]
-        node2.parent = temp_node1[1]
-        node2.lchild = temp_node1[2]
-        node2.rchild = temp_node1[3]
-
-    def _maintain_heap(self):
-        """Bubble sort the heap."""
-        for node in self.node_list:
-            if node.val < node.lchild.val:
-                self._swap_nodes(node, node.lchild)
+        if isinstance(iterable, (str, tuple, list)):
+            self.heap_list = iterable
 
     def push(self, val):
-        """Add a new node to the heap."""
-        self.node_list.append(Node(len(self.node_list), val))
+        """Add new node to Heap."""
+        self.heap_list.append(Node(len(self.heap_list), val))
+        self._sort()
 
-        self._maintain_heap()
+    def swap(parent, child):
+        """Change indices of two nodes."""
+        tmp = parent.index
+        parent.index = child.index
+        child.index = tmp
 
-    def pop(self):
-        """Remove and return the top node in the heap."""
-        temp_node1 = self.node_list[0]
-        temp_node2 = self.node_list[-1]
+        self.heap_list[parent.index] = parent
+        self.heap_list[child.index] = child
 
-        self.node_list[0] = temp_node2
-        self.node_list[0].idx = 0
-        self.node_list[-1] = temp_node1
+    def pop():
+        """Return root node of Heap."""
+        swap(self.heap_list[0], self.heap_list[-1]) #swap root with bottom most child
+        popped = self.heap_list.pop().value
+        self._sort()
+        return popped
 
-        popped_node = self.node_list.pop()
-
-        self._maintain_heap()
-
-        return popped_node
+    def _sort():
+        """Bubble sort on heap."""
+        for i in range(len(self.heap_list)):
+            node = self.heap_list[i]
+            if node.value < self.heap_list[node.left]:
+                swap(node, self.heap_list[node.left])
+                i = 0
+            if node.value < self.heap_list[node.right]:
+                swap(node, self.heap_list[node.right])
+                i = 0

0c5e091464797ee4e19e7083a34200ad1ca050ad
David Franklin <dave5801@gmail.com>
2017-11-04 14:58:06 -0700

    rewrite of queue class using dlinked list, new tests

--- que_.py
+++ que_.py
@@ -1,45 +1,28 @@
 """Define Queue Class."""
 
+
 from doubly_linked_list import DLinkedList
-from doubly_linked_list import Node
 
 
-class Queue(object):
+class Queue(DLinkedList):
     """Queue Class which is composed from Linked List."""
 
     def __init__(self):
         """Constructor for Queue."""
-        self.linked_list = DLinkedList()
+        self.q = DLinkedList()
 
     def __len__(self):
         """Return size of the queue."""
-        return self.linked_list.size()
+        return self.q.size()
 
     def enqueue(self, val):
         """Add new element to end of the queue."""
-        new_node = Node(self, val)
-
-        if self.linked_list.head is None:
-            self.linked_list.head = new_node
-            self.linked_list.tail = new_node
-        else:
-            self.linked_list.tail.prev_node == new_node
-            new_node.next_node = self.linked_list.tail
-            self.linked_list.tail == new_node
+        self.q.append(val)
 
     def dequeue(self):
         """Remove from the end of the queue."""
-        if self.linked_list.head is not None:
-            removed_node = self.linked_list.head
-            self.linked_list.head.prev_node = self.linked_list.head
-            return removed_node.data
-        raise IndexError("List is empty")
+        self.q.shift()
 
     def peek(self):
         """Return value of next element in queue."""
-        return self.linked_list.head.data
-
-    def size(self):
-        """Define the size method."""
-        return self.linked_list.size()
-        
+        return self.q.head.data
--- test_que_.py
+++ test_que_.py
@@ -1,20 +1,63 @@
 """Test the functionality of the Queue class."""
 
 
-def test_queue_exists():
-    """Test that a Queue object is created."""
+def test_empty_queue_is_length_zero():
+    """Test length of empty queue."""
     from que_ import Queue
     q = Queue()
-    assert q
+    assert q.__len__() == 0
 
 
-def test_head_tail_of_list_of_len_1():
-    """Test that in a one-node list, the node is head and tail."""
+def test_enqueue_one_node_return_length_is_one():
+    """Test add one node to queue."""
     from que_ import Queue
     q = Queue()
-    q.enqueue(5)
-    assert q.linked_list.head
-    assert q.linked_list.tail
+    q.enqueue(1)
+    assert q.__len__() == 1
+
+
+def test_enqueu_length_is_len_of_range_of_random_nodes():
+    """Test enqueue of sample space of randoms."""
+    from que_ import Queue
+    import random
+    q = Queue()
+
+    random_nodes = random.sample(range(1, 10), 9)
+    test_len_size = len(random_nodes)
+
+    for i in random_nodes:
+        q.enqueue(1)
+
+    assert q.__len__() == test_len_size
+
+
+def test_deque_empty_queue_raises_error():
+    """Test attempt to dequeue from empty queue."""
+    import pytest
+    from que_ import Queue
+    q = Queue()
+    with pytest.raises(IndexError):
+        q.dequeue()
+
+
+def test_dequeue_after_one_enqueue():
+    """Test dequeue after enqueue."""
+    from que_ import Queue
+    q = Queue()
+    q.enqueue(1)
+    assert q.dequeue() is None
+
+
+def test_dequeue_after_multiple_enqueue():
+    """Test dequeue of range of enqueues."""
+    from que_ import Queue
+    q = Queue()
+    test_nodes = [i for i in range(20)]
+    for i in test_nodes:
+        q.enqueue(i)
+
+    q.dequeue()
+    assert q.__len__() == len(test_nodes)
 
 
 def test_output_of_peek_exists():
@@ -33,12 +76,3 @@ def test_if_peek_needs_no_inputs():
     q.enqueue(5)
     with pytest.raises(TypeError):
         q.peek(5)
-
-
-def test_queue_len_method_is_working():
-    from que_ import Queue
-    q = Queue()
-    q.enqueue(1)
-    q.enqueue(2)
-    q.enqueue(3)
-    assert len(q) == 3

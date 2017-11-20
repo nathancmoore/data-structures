66660acc53dd07acf0149b3fe287e50d9ac3e128
Nathan Moore <ncmoore1986@gmail.com>
2017-11-20 11:48:23 -0800

    wrote the insert method and the hidden recursive method _find_spot

--- bst.py
+++ bst.py
@@ -10,6 +10,10 @@ class Node(object):
         self.left = left
         self.right = right
 
+    def __repr__(self):
+        """Show each node as its value."""
+        return self.val
+
 
 class BST(object):
     """Define the BST-class object."""
@@ -18,9 +22,32 @@ class BST(object):
         """Constructor for the BST class."""
         self.balance = 0
         self.size = 0
+        self.root = None
 
     def balance(self):
         """Return the current balance of the BST."""
         return self.balance
 
-    
+    def insert(self, value):
+        """Insert a new node into the BST."""
+        new_node = Node(value)
+
+        if self.root:
+            self._find_spot(new_node, self.root)
+
+    def _find_spot(self, node_to_add, node_to_check):
+        r""".
+
+        Check if the node_to_add belongs on the left or right\
+         of the node_to_check, then place it there if that spot is empty,\
+          otherwise recur.
+        """
+        if node_to_add > node_to_check:
+            if node_to_check.right:
+                self._find_spot(node_to_add, node_to_check.right)
+            node_to_check.right = node_to_add
+
+        elif node_to_add < node_to_check:
+            if node_to_check.left:
+                self._find_spot(node_to_add, node_to_check.left)
+            node_to_check.left = node_to_add

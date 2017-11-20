e04833b04e46ee871081003bcaeeb871e451bb76
Nathan Moore <ncmoore1986@gmail.com>
2017-11-20 13:03:44 -0800

    added the size method, and polished up the init to let the BST take in an iterable

--- bst.py
+++ bst.py
@@ -18,24 +18,57 @@ class Node(object):
 class BST(object):
     """Define the BST-class object."""
 
-    def __init__(self):
+    def __init__(self, starting_values=None):
         """Constructor for the BST class."""
         self.balance = 0
         self.size = 0
-        self.root = None
+
+        if starting_values is None:
+            self.root = None
+
+        elif isinstance(starting_values, (list, str, tuple, set)):
+            self.root = starting_values[0]
+            self.size += 1
+            for i in len(starting_values) - 1:
+                self.insert(starting_values[i + 1])
+
+        else:
+            raise TypeError('Only iterables or None\
+                are valid parameters!')
 
     def balance(self):
         """Return the current balance of the BST."""
         return self.balance
 
+    def size(self):
+        """Return the current size of the BST."""
+        return self.size
+
     def insert(self, value):
-        """Insert a new node into the BST."""
+        """Insert a new node into the BST, and adjust the balance."""
         new_node = Node(value)
 
         if self.root:
-            self._find_spot(new_node, self.root)
+            if new_node > self.root:
+                self.balance += 1
+                if self.root.right:
+                    self._find_home(new_node, self.root.right)
+                else:
+                    self.root.right = new_node
+                    self.size += 1
 
-    def _find_spot(self, node_to_add, node_to_check):
+            elif new_node < self.root:
+                self.balance -= 1
+                if self.root.left:
+                    self._find_home(new_node, self.root.left)
+                else:
+                    self.root.left = new_node
+                    self.size += 1
+
+        else:
+            self.root = new_node
+
+    def _find_home(self, node_to_add, node_to_check):
         r""".
 
         Check if the node_to_add belongs on the left or right\
@@ -44,10 +77,37 @@ class BST(object):
         """
         if node_to_add > node_to_check:
             if node_to_check.right:
-                self._find_spot(node_to_add, node_to_check.right)
-            node_to_check.right = node_to_add
+                self._find_home(node_to_add, node_to_check.right)
+            else:
+                node_to_check.right = node_to_add
+                self.size += 1
 
         elif node_to_add < node_to_check:
             if node_to_check.left:
-                self._find_spot(node_to_add, node_to_check.left)
-            node_to_check.left = node_to_add
+                self._find_home(node_to_add, node_to_check.left)
+            else:
+                node_to_check.left = node_to_add
+                self.size += 1
+
+    def search(self, value):
+        """Return whether or not a value is in the BST."""
+        return self._check_for_equivalence(value, self.root)
+
+    def _check_for_equivalence(self, value, node_to_check):
+        r""".
+
+        Check if the value matches that of the node_to_check\
+         if it does, return true if it doesn't, go left or right
+         as appropriate and recur. If you reach a dead end, return false.\
+        """
+        if value > node_to_check:
+            if node_to_check.right:
+                self._check_for_equivalence(value, node_to_check.right)
+            else:
+                node_to_check.right = value
+
+        elif value < node_to_check:
+            if node_to_check.left:
+                self._check_for_equivalence(value, node_to_check.left)
+            else:
+                node_to_check.left = value

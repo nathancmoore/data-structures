003fdba30636933ba67f9c6f7098be13b709829f
Nathan Moore <ncmoore1986@gmail.com>
2017-12-07 16:56:12 -0800

    wrote a few tests. not sure what other tests i need

--- bubble.py
+++ bubble.py
@@ -1,19 +1,34 @@
 """Bubble sort."""
 
 
-def bubble_sort(list):
+def bubble_sort(a_list):
     """Use BS to sort the provided list and return it."""
-    output_list = []
-    while list:
-        if len(list) > 1:
-            for i in range(len(list) - 1):
-                if list[i] > list[i + 1]:
-                    list[i], list[i + 1] = list[i + 1], list[i]
-        output_list.append(list.pop(-1))
+    if not isinstance(a_list, list):
+        raise TypeError("Only list is a valid input type!")
 
-    return output_list[::-1]
+    if len(a_list) < 2:
+        return a_list
+
+    loops = 0
+    while True:
+        swaps = 0
+        for i in range(len(a_list) - 1 - loops):
+            if a_list[i] > a_list[i + 1]:
+                a_list[i], a_list[i + 1] = a_list[i + 1], a_list[i]
+                swaps += 1
+        if swaps == 0 or len(a_list) - loops == 2:
+            return a_list
+        loops += 1
 
 if __name__ == '__main__':
     a = [9, 7, 10, 35, 2, 6, 1, 300, 26, 99]
+    b = []
+    c = [6]
+    d = [1, 2, 3, 4, 5, 6]
+    e = [3, 3, 3]
 
     print(bubble_sort(a))
+    print(bubble_sort(b))
+    print(bubble_sort(c))
+    print(bubble_sort(d))
+    print(bubble_sort(e))
